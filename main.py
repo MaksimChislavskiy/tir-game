54ae93ce3747ee3d7ba17b7e5612b289b411a1a3
MaksimChislavskiy <maxwell884884@gmail.com>
2025-05-18 17:25:37 +0300

    add cons-code

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+import  random
 
 pygame.init()
-
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра Тир')
+icon = pygame.image.load('img/free-icon-shooting-range-3548954.png')
+pygame.display.set_icon(icon)
+target_img = pygame.image.load('img/free-icon-shooting-range-3516567.png')
+target_wight = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_wight)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 running = True
 
 while running:
