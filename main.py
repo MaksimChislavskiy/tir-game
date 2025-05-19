8c54510dd2fb77d348ce8c5755965327b5c099b4
MaksimChislavskiy <maxwell884884@gmail.com>
2025-05-19 14:49:30 +0300

    add cycle-code

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import pygame
 
 pygame.init()
+<<<<<<< Updated upstream
 
+=======
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра Тир')
+icon = pygame.image.load('img/free-icon-shooting-range-3548954.png')
+pygame.display.set_icon(icon)
+original_target_img = pygame.image.load('img/free-icon-shooting-range-3516567.png')
+target_width = 80
+target_height = 80
+target_img = pygame.transform.scale(original_target_img, (target_width, target_height))
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+>>>>>>> Stashed changes
 running = True
 
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 
 
